a10c099003d100259c7f89a49f7759ffd8b276b4
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-12-02 17:02:20 +0000

    added array of words for the game and set up a variable to hold a selected word for each round of game

--- script.js
+++ script.js
@@ -8,3 +8,5 @@ const finalMessage = document.getElementById("final-message");
 const figureParts = document.querySelectorAll(".figure-part");
 
 const words = ["application", "programming", "interface", "wizard"];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
