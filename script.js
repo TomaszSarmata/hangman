9e663c15f5d43f1c6a9298bf77b6249389804093
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-12-04 15:45:25 +0000

    added extra code to the updateWrongLettersEl function to check if the user has lost

--- script.js
+++ script.js
@@ -38,17 +38,26 @@ function displayWord() {
 
 //Update the wrong letters
 function updateWrongLettersEl() {
+  //Display wrong letters
   wrongLettersEl.innerHTML = `
   ${wrongLetters.length > 0 ? `<p>Wrong</p>` : ""}
   ${wrongLetters.map((letter) => `<span>${letter}</span>`)}
   `;
 
+  //Display parts/hangman
   figureParts.forEach((part, index) => {
     const errors = wrongLetters.length;
     if (index < errors) {
       part.style.display = "block";
+    } else {
+      part.style.display = "none";
     }
   });
+  //Check if lost
+  if (wrongLetters.length === figureParts.length) {
+    finalMessage.innerText = "Unfortunatelly, you have lost 🥹";
+    popup.style.display = "flex";
+  }
 }
 
 //Show notification
